f6468a0cfe6e91b70b307cef65ec65f7f5977088
uwu <potatocuteuwu@gmail.com>
2026-02-27 10:15:58 +0300

    создание проекта модульного теста, тестового класса и тестов BankAccountTests и lesswhenzero

--- Bank/BankAccount.cs
+++ Bank/BankAccount.cs
@@ -78,7 +78,7 @@ namespace BankAccountNS
         {
             BankAccount ba = new BankAccount("Mr. Roman Abramovich", 11.99);
 
-            ba.Credit(-5.77);
+            ba.Credit(5.77);
             ba.Debit(11.22);
             Console.WriteLine("Current balance is ${0}", ba.Balance);
             Console.ReadLine();
